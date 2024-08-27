d42d4e3bd0123f6502568c419e4788eb9bae11b5
João Pedro <133026499+jp-mmoura@users.noreply.github.com>
2024-08-27 14:25:36 -0300

    Add files via upload

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,10 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js'
+
+import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,27 +18,24 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try{
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-    const data = await res.json();
+    const id = window.location.hash.slice(1);
+    if(!id) return; 
+    recipeView.renderSpinner();
+    renderSpinner(recipeContainer);
+    await model.loadRecipe(id);
 
-    if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+    recipeView.render(model.state.recipe);
+    const recipeView = new recipeView(model.state.recipe);
 
-    let {recipe} = data.data.recipe;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceURL: recipe.source_url, 
-      image: recipe.image_url, 
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients
-    };
-    console.log(recipe);
   }catch(err){
     alert(err);
   }
 }
-showRecipe();
+
+arr = ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+/* The array does the same as this two lines of code here:
+window.addEventListener('hashchange', showRecipe);
+window.addEventListener('load', showRecipe);
+*/
